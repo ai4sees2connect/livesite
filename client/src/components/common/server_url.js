7014a9e327b11ae7a1167c7268892af27f5ce506
jai-chawla <jaichawla074@gmail.com>
2024-09-07 13:34:51 +0530

    fixes

--- client/src/components/common/server_url.js
+++ client/src/components/common/server_url.js
@@ -1,6 +1,6 @@
 // const api='https://clone-internshub-api.vercel.app' 
 
-const api='https://clone-internshub-api.vercel.app' 
+const api='https://livesite-2s9u.vercel.app' 
 // const api='http://localhost:4000'
 // const api='http://localhost:4000'
 
