6f6d941d99abc0c3f513112f34a0d8ea46d8efc4
jai-chawla <jaichawla074@gmail.com>
2024-10-23 10:45:59 +0530

    pushing to seperate branch

--- client/src/components/common/server_url.js
+++ client/src/components/common/server_url.js
@@ -1,9 +1,9 @@
 
 
 
-const api='http://localhost:4000'
-// const api='https://livesite-1y3j.onrender.com'
-
+// const api='http://localhost:4000'
+const api='https://livesite-1y3j.onrender.com'
+//
 
 
 export default api;
