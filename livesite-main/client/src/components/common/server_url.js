5f9ab390ff8657851455ae85eede1e8af8980f04
pushkar106 <pushkarrajput108@gmail.com>
2026-06-05 11:30:57 +0530

    Remove livesite-main folder

--- livesite-main/client/src/components/common/server_url.js
+++
@@ -1,6 +0,0 @@
-const api='http://localhost:4000'
-
-// const api = "https://livesite-backend-74ut.onrender.com";
-
-
-export default api;
--- livesite-main/client/src/components/student/InternDetails.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InternDetails = () => {
-  return (
-    <div>InternDetails</div>
-  )
-}
-
-export default InternDetails
